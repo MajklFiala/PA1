e850bcc86cb97e961b312269bee8c139c81b7d4d
Michael Fiala <mfiala2003@gmail.com>
2024-01-10 14:59:55 +0100

    Second function addSetCell

--- hmw08.c
+++ hmw08.c
@@ -35,7 +35,156 @@ void initMatrix   ( TSPARSEMATRIX   * m )
 
 void addSetCell   ( TSPARSEMATRIX   * m, int rowIdx,int  colIdx,int  data ) 
 {
+    TCELL *new_cell = (TCELL*)malloc(sizeof(*new_cell));
+  new_cell->m_Row = rowIdx;
+  new_cell->m_Col = colIdx;
+  new_cell->m_Data = data;
 
+  //create colums linked list struct TROWCOL
+
+  TROWCOL *col = (TROWCOL*)malloc(sizeof(*col));
+  col->m_Idx = colIdx;
+
+  TROWCOL *cols = m->m_Cols;
+  
+
+  //if there is no colums linked list or there is then go through it
+  if(cols == nullptr){
+    col->m_Next = nullptr;
+    new_cell->m_Down = nullptr;
+    new_cell->m_Right = nullptr;
+    col->m_Cells = new_cell;
+    m->m_Cols = col;
+  }else{
+    TROWCOL *col_prev = nullptr;
+  TCELL *cell_prev = nullptr;
+
+  //going through the colum linked list 
+  while(cols != nullptr){
+    if(cols->m_Idx == colIdx){
+      //going through the cell linked list in the specific column
+      TCELL *cell = cols->m_Cells;
+      while(cell != nullptr){
+        //check if the row and col already exists and only change the data
+        if(cell->m_Row == rowIdx){
+          cell->m_Data = data;
+          break;
+        }else if(cell->m_Row <  rowIdx){
+          if(cell->m_Down == nullptr){
+            new_cell->m_Down = nullptr;
+            cell->m_Down = new_cell;
+            break;
+          }
+          cell_prev = cell;
+        }else if(cell->m_Row > rowIdx){
+          new_cell->m_Down = cell;
+          if(cell_prev != nullptr){
+            cell_prev->m_Down = new_cell;
+          }else{
+            cols->m_Cells = new_cell;
+          }
+          break;
+        }
+        cell = cell->m_Down;
+        
+      }
+      free(col);
+      break;
+    }else if(cols->m_Idx < colIdx){
+      if(cols->m_Next == nullptr){
+        new_cell->m_Down = nullptr;
+        col->m_Cells = new_cell;
+        col->m_Next = nullptr;
+        cols->m_Next = col;
+        break;
+      }
+      col_prev = cols;
+    }else if(cols->m_Idx > colIdx){
+      new_cell->m_Down = nullptr;
+      col->m_Cells = new_cell;
+      col->m_Next = cols;
+      if(col_prev != nullptr){
+        col_prev->m_Next = col;
+      }else{
+        m->m_Cols = col;
+      }
+      break;
+    }
+    cols = cols->m_Next;
+  }
+  }
+
+  //creates the row linke dlist for struct TROWCOL
+  TROWCOL *row = (TROWCOL*)malloc(sizeof(*row));
+  row->m_Idx = rowIdx;
+
+
+  TROWCOL *rows = m->m_Rows;
+
+  //if row list exists or not
+  if(rows == nullptr){
+    row->m_Next = nullptr;
+    row->m_Cells = new_cell;
+    m->m_Rows = row;
+  }else{
+    TROWCOL *row_prev = nullptr;
+    TCELL *cell_prev2 = nullptr;
+
+    //loop through the row list
+    while(rows != nullptr){
+      if(rows->m_Idx == rowIdx){
+          //loop through the cell list iin the specisif row list
+          TCELL *cell2 =rows->m_Cells;
+          while(cell2 != nullptr){
+            if(cell2->m_Col == colIdx){
+              cell2->m_Data = data;
+              free(new_cell);
+              break;
+            }else if(cell2->m_Col < colIdx){
+              if(cell2->m_Right == nullptr){
+                new_cell->m_Right = nullptr;
+                cell2->m_Right = new_cell;
+                break;
+              }
+              cell_prev2 = cell2;
+            }else if(cell2->m_Col > colIdx){
+              new_cell->m_Right = cell2;
+              if(cell_prev2 != nullptr){
+                cell_prev2->m_Right = new_cell;
+              }else{
+                rows->m_Cells = new_cell;
+              }
+              break;
+            }
+            cell2 = cell2->m_Right;
+          }
+          free(row);
+        break;
+
+      }else if(rows->m_Idx < rowIdx){
+        if(rows->m_Next == nullptr){
+          new_cell->m_Right = nullptr;
+          row->m_Cells = new_cell;
+          row->m_Next = nullptr;
+          rows->m_Next = row;
+          break;
+        }
+        row_prev = rows;
+      }else if(rows->m_Idx > rowIdx){
+        new_cell->m_Right = nullptr;
+        row->m_Cells = new_cell;
+        row->m_Next = rows;
+        if(row_prev != nullptr){
+          row_prev->m_Next = row;
+        }else{
+          m->m_Rows = row;
+        }
+        break;
+
+      }
+      rows = rows->m_Next;
+    }
+  }
 }
 
 bool removeCell   ( TSPARSEMATRIX   * m,int rowIdx,int colIdx )
@@ -45,7 +194,7 @@ bool removeCell   ( TSPARSEMATRIX   * m,int rowIdx,int colIdx )
 
 void freeMatrix   ( TSPARSEMATRIX   * m )
 {
-    
+
 }
 
 #ifndef __PROGTEST__
